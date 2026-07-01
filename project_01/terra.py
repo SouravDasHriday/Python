77956defd66f8d5cbc54557d202b1014e7b55e02
Ubuntu <ubuntu@ip-172-31-34-144.ec2.internal>
2026-07-02 00:10:49 +0600

    update output status in terra.py file

--- project_01/terra.py
+++ project_01/terra.py
@@ -1,9 +1,8 @@
-# importing a library
 import subprocess
-# Path where the Terraform files are located:
-TERRAFORM_DIRECTORY = "/home/ubuntu/PYTHON_PRACTICE/terraform-automation/terraform"
 
-# declering a function to run terraform commands:
+TERRAFORM_DIRECTORY = "/home/ubuntu/PYTHON_PRACTICE/project_01/terraform-automation/terraform"
+
+
 def run_terraform(command):
     result = subprocess.run(
         command,
@@ -14,14 +13,21 @@ def run_terraform(command):
     )
     return result
 
-# callign the function:
-result = run_terraform(["terraform", "init"])
-#result = run_terraform(["terraform", "plan"])
-#result = run_terraform(["terraform", "apply", "-auto-approve"])
-#result = run_terraform(["terraform", "destroy", "-auto-approve"])
-print(result.stdout)
 
+try:
+#    result = run_terraform(["terraform", "init"])
+#    result = run_terraform(["terraform", "plan"])
+#    result = run_terraform(["terraform", "apply", "-auto-approve"])
+    result = run_terraform(["terraform", "destroy", "-auto-approve"])
+    print(result.stdout)
 
+except subprocess.CalledProcessError as error:
+    print("Terraform command failed!\n")
 
+    print("Return Code:", error.returncode)
 
+    print("\n----- STDOUT -----")
+    print(error.stdout)
 
+    print("\n----- STDERR -----")
+    print(error.stderr)
