64a7d9277a7ffe96bf6b5ccbe396b2fd7e55f188
Ubuntu <ubuntu@ip-172-31-34-144.ec2.internal>
2026-07-02 00:09:02 +0600

    updated some configuration on terraform

--- project_01/terraform-automation/terraform/ec2.tf
+++ project_01/terraform-automation/terraform/ec2.tf
@@ -52,7 +52,9 @@ resource "aws_instance" "testinstance" {
   ami             = var.ami_id
   instance_type   = var.instance_type
   key_name        = aws_key_pair.deployer.key_name
-  security_groups = [aws_security_group.allow_user_to_connect.name]
+  vpc_security_group_ids = [
+  aws_security_group.allow_user_to_connect.id
+]
   tags = {
     Name = "Automate"
   }
